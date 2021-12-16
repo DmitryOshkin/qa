3836326eabca0332463105878145b178a6ee91fa
DmitryOshkin <46111421+DmitryOshkin@users.noreply.github.com>
2021-12-16 23:00:28 +0300

    added TextBoxTests

---
+++ src/test/java/yandex/oshkin/TextBoxTests.java
@@ -0,0 +1,36 @@
+package yandex.oshkin;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TextBoxTests {
+//    @BeforeAll
+//    static void beforeAll() {
+//        Configuration.browserSize ="1920x1080";
+//    }
+
+    @Test
+    void fillFormTest() {
+        open("https://demoqa.com/text-box");
+        $("#userName").setValue("Some name");
+        $("[id=userEmail]").setValue("111@333.com");
+        $("#currentAddress").setValue("Some address");
+        $("#permanentAddress").setValue("Another address");
+        $("#submit").scrollTo().click();
+
+        $("#output #name").shouldHave(text("Some name"));
+        $("#output").$("#email").shouldHave(text("111@333.com"));
+        $("#output").$("#currentAddress").shouldHave(text("Some address"));
+        $("#output").$("#permanentAddress").shouldHave(text("Another address"));
+
+
+//        $("#output").shouldHave(text("Some name"),text("111@333.com"),
+//               text("Some address"),text("Another address"));
+    }
+
+}
